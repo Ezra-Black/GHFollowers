aef031e2749f9d0afb43edbc4cec962ffe1da712
Ezra Black <dev.ezrablack@gmail.com>
2020-11-28 17:57:28 -0600

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Ezra Black on 11/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
