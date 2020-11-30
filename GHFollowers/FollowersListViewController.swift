97294186daf1e46cffec3ec998f51e156c28afa0
Ezra Black <dev.ezrablack@gmail.com>
2020-11-30 10:45:10 -0600

    Networking implemented

--- GHFollowers/FollowersListViewController.swift
+++ GHFollowers/FollowersListViewController.swift
@@ -15,6 +15,16 @@ class FollowersListViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(title: "Ope! Bad stuff happened.", message: errorMessage!, buttonTitle: "Ok")
+                return
+            }
+            
+            print("followers.count = \(followers.count)")
+            print(followers)
+        }
     }
     
     
