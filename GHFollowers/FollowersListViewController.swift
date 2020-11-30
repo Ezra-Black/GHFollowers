df5f4e80d97387c871425274c43e16e0705610d2
Ezra Black <dev.ezrablack@gmail.com>
2020-11-30 16:03:22 -0600

    Added Empty Screen State View

--- GHFollowers/FollowersListViewController.swift
+++ GHFollowers/FollowersListViewController.swift
@@ -59,6 +59,11 @@ class FollowersListViewController: UIViewController {
             case .success(let followers):
                 if followers.count < 100 { self.hasMoreFollowers = false }
                 self.followers.append(contentsOf: followers)
+                
+                if self.followers.isEmpty {
+                    let message = "This user doesn't have any followers, Go follow them 🥺."
+                    DispatchQueue.main.async { self.showEmptyStateView(with: message, in: self.view) }
+                }
                 self.updateData()
                 
             case .failure(let error):
