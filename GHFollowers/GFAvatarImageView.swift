3e17b11c30a441a6c789f693a917f29dbbef9e98
Ezra Black <dev.ezrablack@gmail.com>
2020-11-30 11:48:53 -0600

    Cell implementation

---
+++ GHFollowers/GFAvatarImageView.swift
@@ -0,0 +1,35 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Ezra Black on 11/30/20.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+//    func tamic(_ view: UIView) {
+//        view.translatesAutoresizingMaskIntoConstraints = false
+//    }
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds      = true
+        image              = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
