a193ba0abe492a120e130099525a8c04445dfd31
Ezra Black <dev.ezrablack@gmail.com>
2020-11-30 00:00:39 -0600

    Added Reusable Button

---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,37 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Ezra Black on 11/29/20.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("⚠️init(coder:) has not been implemented⚠️")
+    }
+    
+    
+    init(backroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    
+    private func configure() {
+        layer.cornerRadius      = 10
+        titleLabel?.textColor   = .white
+        titleLabel?.font        = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
