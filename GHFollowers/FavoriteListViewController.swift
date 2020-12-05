c09de677bc2153f93ee37db4d8d4a0a99a10acec
Ezra Black <dev.ezrablack@gmail.com>
2020-12-04 23:48:23 -0600

    error fix

--- GHFollowers/FavoriteListViewController.swift
+++ GHFollowers/FavoriteListViewController.swift
@@ -9,15 +9,29 @@ import UIKit
 
 class FavoriteListViewController: UIViewController {
 
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        view.backgroundColor = .systemBackground
+//        navigationController?.navigationBar.prefersLargeTitles = true
+//    }
+//
+//
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        navigationController?.setNavigationBarHidden(false, animated: true)
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.prefersLargeTitles = true
-    }
-    
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: true)
+        view.backgroundColor = .systemBlue
+        
+        PersistenceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+            case .failure(let error):
+                break
+            }
+        }
     }
 }
--- GHFollowers/PersistenceManager.swift
+++ GHFollowers/PersistenceManager.swift
@@ -33,7 +33,7 @@ enum PersistenceManager {
                 case .remove:
                     retrievedFavorites.removeAll { $0.login == favorite.login }
                 }
-                completion(save(favorites: favorites))
+                completion(save(favorites: retrievedFavorites))
             case .failure(let error):
                 completion(error)
             }
