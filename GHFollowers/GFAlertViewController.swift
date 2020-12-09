1efb5bfe8e9b3a7cd54b6dfc158b53ec3eda7341
Ezra Black <dev.ezrablack@gmail.com>
2020-12-08 20:58:24 -0600

    GFAlertViewController Refactored to its own container view file - removing the instances need to know the configuration

--- GHFollowers/GFAlertViewController.swift
+++ GHFollowers/GFAlertViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class GFAlertViewController: UIViewController {
     
-    let containerView   = UIView()
+    let containerView   = GFAlertContainerView()
     let titleLabel      = GFTitleLabel(textAlignment: .center, fontSize: 20)
     let messageLabel    = GFBodyLabel(textAlignment: .center)
     let actionButton    = GFButton(backgroundColor: .systemPink, title: "Ok")
@@ -36,7 +36,7 @@ class GFAlertViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.75)
         configureContainerView()
         configureTitleLabel()
         configureActionButton()
@@ -46,11 +46,6 @@ class GFAlertViewController: UIViewController {
     
     func configureContainerView() {
         view.addSubview(containerView)
-        containerView.backgroundColor       = .systemBackground
-        containerView.layer.cornerRadius    = 16
-        containerView.layer.borderWidth     = 2
-        containerView.layer.borderColor     = UIColor.white.cgColor
-        containerView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
